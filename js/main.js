2eb0bc1f637aeafea2d92f4c1585cd0dc630c09f
MUAZ <muazwahbi01@gmail.com>
2024-09-04 05:51:51 -0700

    starting the project

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// window.addEventListener("resize",function(){
+//     location.reload
+// })
+// const header = document.querySelector("header")
+// let lsp = window.scrollY
+// window.addEventListener("scroll", function(){
+//     const stp = window.scrollY
+//     if (stp > lsp) {
+//         if (lsp > 150) {
+//             header.style.cssText = "transform: translateY(-100%);"
+//         }
+//     }else if (stp < lsp) {
+//         header.style.cssText = "transform: translateY(0%);"
+//     }
+//     lsp = stp
+// })
+// header animation
+const i = document.querySelector("header i");
+const nav = document.querySelector("header nav");
+i.onclick = () => {
+    nav.classList.toggle("navanimation")
+};
+const nava = document.querySelectorAll("nav a")
+nava.forEach(a =>{
+    a.onclick = () =>{
+        nav.classList.remove("navanimation")
+    } 
+})
+document.addEventListener("click", function(e){
+    if (e.target.className != "navanimation" && e.target != i){
+        nav.classList.remove("navanimation")
+    }
+})
